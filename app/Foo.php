8b900d23f8b4f2184242fdc9ccf4f1ab632597af
raysuke <kurosuke05185@yahoo.co.jp>
2019-12-13 16:33:25 +0900

    first commit

---
+++ app/Foo.php
@@ -0,0 +1,13 @@
+<?php
+class Foo
+{
+	public function addition($num1, $num2)
+	{
+		return $num1 + $num2;
+	}
+
+	public function subtraction($num1, $num2)
+	{
+		return $num1 - $num2;
+	}
+}
---
+++ tests/FooTest.php
@@ -0,0 +1,19 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require('app/Foo.php');
+
+class FooTest extends TestCase
+{
+
+	public function testAddition()
+	{
+		$Foo = new Foo();
+		$this->assertEquals(5, $Foo->addition(3, 2));
+	}
+
+	public function testSubtraction()
+	{
+		$Foo = new Foo();
+		$this->assertEquals(1, $Foo->subtraction(3, 2));
+	}
+}
